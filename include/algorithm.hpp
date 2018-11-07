03da5ef35ba4c1a93898174b6e5ce7e97b3b5636
Bob Carter <bobcarterirl@gmail.com>
2018-11-07 16:28:09 -0600

    Add function prototypes, switch to prefix increment

--- include/algorithm.hpp
+++ include/algorithm.hpp
@@ -3,11 +3,20 @@
 namespace hbstl
 {
 
+template<typename II1, typename II2>
+bool equal(II1 first1, II1 last1, II2 first2);
+
+template<typename II1, typename II2>
+bool lexicographical_compare(II1 first1, II1 last1, II2 first2, II2 last2);
+
+
+// Template function implementations
+
 // TODO predicate
 template<typename II1, typename II2>
 bool equal(II1 first1, II1 last1, II2 first2)
 {
-    for (; first1 != last1; first1++, first2++)
+    for (; first1 != last1; ++first1, ++first2)
     {
         if (!(*first1 == *first2)) return false;
     }
@@ -19,7 +28,7 @@ bool equal(II1 first1, II1 last1, II2 first2)
 template<typename II1, typename II2>
 bool lexicographical_compare(II1 first1, II1 last1, II2 first2, II2 last2)
 {
-    for (; first1 != last1 && first2 != last2; first1++, first2++)
+    for (; first1 != last1 && first2 != last2; ++first1, ++first2)
     {
         if (*first1 < *first2) return true;
         else if (*first2 < *first1) return false;
